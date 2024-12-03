1a4e3902eefec41da9bcdaae1912d72409db7bc4
Ekin A <ekinaltin62@gmail.com>
2024-12-03 23:42:35 +0300

    Update

--- public/assets/js/cardSearch.js
+++ public/assets/js/cardSearch.js
@@ -1,30 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const cardSearchInput = document.querySelector('.card-search-input');
-  const clearCardSearchBtn = document.querySelector('.clear-card-search-btn');
-  const cards = document.querySelectorAll('.card');
+  try {
+    const cardSearchInput = document.querySelector('.card-search-input');
+    const clearCardSearchBtn = document.querySelector('.clear-card-search-btn');
+    const cards = document.querySelectorAll('.card');
 
-  cardSearchInput.addEventListener('input', () => {
-    const filterText = cardSearchInput.value.toLowerCase();
+    cardSearchInput.addEventListener('input', () => {
+      const filterText = cardSearchInput.value.toLowerCase();
 
-    cards.forEach((card) => {
-      let isMatched = false;
+      cards.forEach((card) => {
+        let isMatched = false;
 
-      if (card.textContent.toLowerCase().includes(filterText)) {
-        isMatched = true;
-      }
+        if (card.textContent.toLowerCase().includes(filterText)) {
+          isMatched = true;
+        }
 
-      if (isMatched) {
+        if (isMatched) {
+          card.style.display = '';
+        } else {
+          card.style.display = 'none';
+        }
+      });
+    });
+
+    clearCardSearchBtn.addEventListener('click', () => {
+      cardSearchInput.value = '';
+      cards.forEach((card) => {
         card.style.display = '';
-      } else {
-        card.style.display = 'none';
-      }
+      });
     });
-  });
-
-  clearCardSearchBtn.addEventListener('click', () => {
-    cardSearchInput.value = '';
-    cards.forEach((card) => {
-      card.style.display = '';
-    });
-  });
+  } catch (err) {}
 });
