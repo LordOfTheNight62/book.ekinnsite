c650fb07fa33afebc649255bf8b6bb102f264026
Ekin A <ekinaltin62@gmail.com>
2024-12-03 03:04:02 +0300

    Update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,5 @@
+const bookController = require('../controllers/bookController');
+
 // Oturum kontrol middleware'i
 exports.isLoggedIn = (req, res, next) => {
   if (req.session.userId) {
@@ -27,6 +29,29 @@ exports.isAdmin = (req, res, next) => {
   return res.status(403).render('error/error.ejs', {
     title: '403',
     statusCode: '403',
-    message: 'Sanırım yanlış geldiniz :)',
+    message: 'Erişim Reddedildi',
   });
+};
+
+exports.checkBookOwnerShip = async (req, res, next) => {
+  try {
+    const book = await bookController.getBookByID(req, res);
+
+    if (book.user_id === req.session.userId) {
+      return next();
+    }
+
+    return res.status(403).render('error/error.ejs', {
+      title: '403',
+      statusCode: '403',
+      message: 'Erişim Reddedildi',
+    });
+  } catch (err) {
+    console.error('Kitap sahipliği kontrol hatası:', err.message);
+    return res.status(500).render('error/error.ejs', {
+      title: '500',
+      statusCode: '500',
+      message: 'Sunucu Hatası',
+    });
+  }
 };
--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -3,16 +3,16 @@ const router = express.Router();
 
 const siteController = require('../controllers/siteController');
 const bookController = require('../controllers/bookController');
-const { isLoggedIn, isAdmin } = require('../middlewares/auth');
+const { isLoggedIn, isAdmin, checkBookOwnerShip } = require('../middlewares/auth');
 
 router.get('/books', siteController.getBooksPage);
 router.get('/books/search', siteController.getSearchPage);
 router.post('/books/search', siteController.searchBookByTerm);
 router.get('/books/add-book', isLoggedIn, bookController.getAddNewBookPage);
 router.post('/books/add-book', isLoggedIn, bookController.addNewBook);
-router.get('/books/edit-book/:id', bookController.getEditBookPage);
-router.post('/books/edit-book/:id', bookController.updateBookByID);
-router.get('/books/delete-book/:id', bookController.getDeleteBookPage);
+router.get('/books/edit-book/:id', checkBookOwnerShip, bookController.getEditBookPage);
+router.post('/books/edit-book/:id', checkBookOwnerShip, bookController.updateBookByID);
+router.get('/books/delete-book/:id', checkBookOwnerShip, bookController.getDeleteBookPage);
 router.get('/books/:id', siteController.getBookDetailPage);
 
 router.use('/admin-panel', isLoggedIn, isAdmin);
