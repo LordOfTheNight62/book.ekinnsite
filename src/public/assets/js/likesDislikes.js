8e9bacf645e04f853108de3a812994bf5e8c2dfb
Ekin A <ekinaltin62@gmail.com>
2024-12-14 04:49:01 +0300

    fixed bugs

--- src/public/assets/js/likesDislikes.js
+++ src/public/assets/js/likesDislikes.js
@@ -9,12 +9,12 @@ document.addEventListener('DOMContentLoaded', () => {
       const dislikeCount = dislikeBtn.querySelector('.dislike-count');
 
       const likeData = {
-        userID: document.querySelector('[data-user-id]').getAttribute('data-user-id') || null,
+        userID: document.querySelector('[data-user-id]').getAttribute('data-user-id'),
         commentID: likeBtn.closest('.card').getAttribute('data-comment-id'),
         isLike: true,
       };
 
-      if (userID) {
+      if (likeData.userID) {
         fetch('/api/add-like', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
@@ -33,18 +33,20 @@ document.addEventListener('DOMContentLoaded', () => {
             dislikeCount.innerText = `${data.statistics.totalCommentDislikes}`;
           })
           .catch((err) => console.error('Hata: ', err));
-      } else if (e.target.closest('.add-dislike-btn')) {
-        const dislikeBtn = e.target.closest('.add-dislike-btn');
-        const likeBtn = dislikeBtn.parentElement.querySelector('.add-like-btn');
-        const dislikeCount = dislikeBtn.querySelector('.dislike-count');
-        const likeCount = likeBtn.querySelector('.like-count');
+      }
+    } else if (e.target.closest('.add-dislike-btn')) {
+      const dislikeBtn = e.target.closest('.add-dislike-btn');
+      const likeBtn = dislikeBtn.parentElement.querySelector('.add-like-btn');
+      const dislikeCount = dislikeBtn.querySelector('.dislike-count');
+      const likeCount = likeBtn.querySelector('.like-count');
 
-        const dislikeData = {
-          userID: document.querySelector('[data-user-id]').getAttribute('data-user-id'),
-          commentID: dislikeBtn.closest('.card').getAttribute('data-comment-id'),
-          isLike: false,
-        };
+      const dislikeData = {
+        userID: document.querySelector('[data-user-id]').getAttribute('data-user-id'),
+        commentID: dislikeBtn.closest('.card').getAttribute('data-comment-id'),
+        isLike: false,
+      };
 
+      if (dislikeData.userID) {
         fetch('/api/add-dislike', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
