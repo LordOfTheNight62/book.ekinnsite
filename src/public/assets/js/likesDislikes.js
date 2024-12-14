fbffa8ce4df419895ce5fbd6b0df018a4165be87
Ekin A <ekinaltin62@gmail.com>
2024-12-14 04:19:13 +0300

    fixed bugs

--- src/public/assets/js/likesDislikes.js
+++ src/public/assets/js/likesDislikes.js
@@ -10,59 +10,61 @@ document.addEventListener('DOMContentLoaded', () => {
       const dislikeCount = dislikeBtn.querySelector('.dislike-count');
 
       const likeData = {
-        userID: document.querySelector('[data-user-id]').getAttribute('data-user-id'),
+        userID: document.querySelector('[data-user-id]').getAttribute('data-user-id') || null,
         commentID: likeBtn.closest('.card').getAttribute('data-comment-id'),
         isLike: true,
       };
 
-      fetch('/api/add-like', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(likeData),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.deletedLike) {
-            likeBtn.querySelector('.bi').className = 'bi bi-hand-thumbs-up text-primary';
-          } else {
-            likeBtn.querySelector('.bi').className = 'bi bi-hand-thumbs-up-fill text-primary';
-          }
-          dislikeBtn.querySelector('.bi').className = 'bi bi-hand-thumbs-down text-primary';
-
-          likeCount.innerText = `${data.statistics.totalCommentLikes}`;
-          dislikeCount.innerText = `${data.statistics.totalCommentDislikes}`;
+      if (userID) {
+        fetch('/api/add-like', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(likeData),
         })
-        .catch((err) => console.error('Hata: ', err));
-    } else if (e.target.closest('.add-dislike-btn')) {
-      const dislikeBtn = e.target.closest('.add-dislike-btn');
-      const likeBtn = dislikeBtn.parentElement.querySelector('.add-like-btn');
-      const dislikeCount = dislikeBtn.querySelector('.dislike-count');
-      const likeCount = likeBtn.querySelector('.like-count');
-
-      const dislikeData = {
-        userID: document.querySelector('[data-user-id]').getAttribute('data-user-id'),
-        commentID: dislikeBtn.closest('.card').getAttribute('data-comment-id'),
-        isLike: false,
-      };
-
-      fetch('/api/add-dislike', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(dislikeData),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.deletedDislike) {
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.deletedLike) {
+              likeBtn.querySelector('.bi').className = 'bi bi-hand-thumbs-up text-primary';
+            } else {
+              likeBtn.querySelector('.bi').className = 'bi bi-hand-thumbs-up-fill text-primary';
+            }
             dislikeBtn.querySelector('.bi').className = 'bi bi-hand-thumbs-down text-primary';
-          } else {
-            dislikeBtn.querySelector('.bi').className = 'bi bi-hand-thumbs-down-fill text-primary';
-          }
-          likeBtn.querySelector('.bi').className = 'bi bi-hand-thumbs-up text-primary';
 
-          likeCount.innerText = `${data.statistics.totalCommentLikes}`;
-          dislikeCount.innerText = `${data.statistics.totalCommentDislikes}`;
+            likeCount.innerText = `${data.statistics.totalCommentLikes}`;
+            dislikeCount.innerText = `${data.statistics.totalCommentDislikes}`;
+          })
+          .catch((err) => console.error('Hata: ', err));
+      } else if (e.target.closest('.add-dislike-btn')) {
+        const dislikeBtn = e.target.closest('.add-dislike-btn');
+        const likeBtn = dislikeBtn.parentElement.querySelector('.add-like-btn');
+        const dislikeCount = dislikeBtn.querySelector('.dislike-count');
+        const likeCount = likeBtn.querySelector('.like-count');
+
+        const dislikeData = {
+          userID: document.querySelector('[data-user-id]').getAttribute('data-user-id'),
+          commentID: dislikeBtn.closest('.card').getAttribute('data-comment-id'),
+          isLike: false,
+        };
+
+        fetch('/api/add-dislike', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(dislikeData),
         })
-        .catch((err) => console.error('Hata: ', err));
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.deletedDislike) {
+              dislikeBtn.querySelector('.bi').className = 'bi bi-hand-thumbs-down text-primary';
+            } else {
+              dislikeBtn.querySelector('.bi').className = 'bi bi-hand-thumbs-down-fill text-primary';
+            }
+            likeBtn.querySelector('.bi').className = 'bi bi-hand-thumbs-up text-primary';
+
+            likeCount.innerText = `${data.statistics.totalCommentLikes}`;
+            dislikeCount.innerText = `${data.statistics.totalCommentDislikes}`;
+          })
+          .catch((err) => console.error('Hata: ', err));
+      }
     }
   });
 });
