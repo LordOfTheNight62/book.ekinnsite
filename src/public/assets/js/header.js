452098d948a2c21a036665e32eda60f201a8b6a1
Ekin A <ekinaltin62@gmail.com>
2024-12-30 02:33:43 +0300

    update

--- src/public/assets/js/header.js
+++ src/public/assets/js/header.js
@@ -7,4 +7,24 @@ document.addEventListener('DOMContentLoaded', () => {
     if (currentPath === linkPath) link.classList.add('active');
     else link.classList.remove('active');
   });
+
+  const headerSearchBtn = document.querySelectorAll('.header-search-btn');
+
+  headerSearchBtn.forEach((searchBtn) => {
+    searchBtn.addEventListener('click', function () {
+      let query = searchBtn.parentElement.querySelector('.header-search-input').value;
+      console.log(query);
+      if (query) {
+        window.location.href = '/books/search?q=' + encodeURIComponent(query);
+      }
+    });
+  });
+
+  document.querySelectorAll('.header-search-btn').addEventListener('click', function () {
+    let query = document.querySelector('.header-search-input').value;
+    console.log(query);
+    if (query) {
+      window.location.href = '/books/search?q=' + encodeURIComponent(query);
+    }
+  });
 });
